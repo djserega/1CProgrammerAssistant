01e1daa600fec41fdb3f4e28e15ab76092468992
djserega <galyuk.sergey@gmail.com>
2018-12-17 22:51:06 +0200

    added ProcessTextInClipboardEvents

---
+++ src/1CProgrammerAssistant/1CProgrammerAssistant/ProcessTextInClipboardEvents.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1CProgrammerAssistant
+{
+    public delegate void ProcessTextInClipboardEvent();
+    public class ProcessTextInClipboardEvents : EventArgs
+    {
+        public static event ProcessTextInClipboardEvent ProcessTextInClipboardEvent;
+        public static void EvokeProcess() => ProcessTextInClipboardEvent?.Invoke();
+    }
+}
