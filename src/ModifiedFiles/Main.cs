6fdefbd538483059107ee6b5d2da9fa134420a6a
djserega <galyuk.sergey@gmail.com>
2019-01-13 14:25:35 +0200

    check hash version

--- src/ModifiedFiles/Main.cs
+++ src/ModifiedFiles/Main.cs
@@ -1,53 +1,30 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Text;
-using System.Threading;
 
 namespace ModifiedFiles
 {
     public class Main
     {
-        private readonly DirectoryInfo _directoryVersion = new DirectoryInfo($"Modified files (versions)");
         private List<Models.File> _files = new List<Models.File>();
+
+        private readonly DirectoryInfo _directoryVersion = new DirectoryInfo($"Modified files (versions)");
         private readonly Dictionary<string, int> _idFileByPath = new Dictionary<string, int>();
-        private readonly Watcher watcher = new Watcher();
+        private readonly Watcher _watcher = new Watcher();
+        private readonly Version _version = new Version();
 
         public Main()
         {
             if (!_directoryVersion.Exists)
                 _directoryVersion.Create();
 
+
+
             Events.CreateNewVersionEvent.CreateNewVersionEvents += (FileInfo fileInfoNewVersion) =>
             {
-                string pathVersion = Path.Combine(
-                    _files[_idFileByPath[fileInfoNewVersion.FullName]].DirectoryVersion,
-                    $"version_{DateTime.Now.ToString("yyyyMMddHHmmss")}");
-
-                pathVersion = $"{pathVersion}{fileInfoNewVersion.Extension}";
-
-                FileInfo fileInfoVersion = new FileInfo(pathVersion);
-                if (!fileInfoVersion.Exists)
-                {
-                    bool copied = false;
-                    for (int i = 0; i < 3; i++)
-                    {
-                        try
-                        {
-                            fileInfoNewVersion.CopyTo(pathVersion);
-                            copied = true;
-                        }
-                        catch (Exception)
-                        {
-                            Thread.Sleep(1000);
-                        }
-
-                        if (copied)
-                            break;
-                    }
-                }
+                _version.CreateNewVersion(fileInfoNewVersion, _files[_idFileByPath[fileInfoNewVersion.FullName]].DirectoryVersion);
             };
         }
 
@@ -63,12 +40,12 @@ namespace ModifiedFiles
                 file.DirectoryVersion = _directoryVersion.CreateSubdirectory(file.FileNameWithoutExtension).FullName;
                 _idFileByPath.Add(file.Path, i);
 
-                watcher.Subscribe(file);
+                _watcher.Subscribe(file);
 
                 i++;
             }
 
-            watcher.RemoveUnusedFiles(_files);
+            _watcher.RemoveUnusedFiles(_files);
         }
 
         public void OpenDirectoryVersion()
---
+++ src/ModifiedFiles/Version.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+
+namespace ModifiedFiles
+{
+    internal class Version
+    {
+        /// <summary>
+        /// Dictionary version: hash -> name file version
+        /// </summary>
+        private readonly Dictionary<string, Dictionary<string, string>> _controlHash = new Dictionary<string, Dictionary<string, string>>();
+
+        internal bool CheckHash { get; set; } = true;
+
+        internal void CreateNewVersion(FileInfo file, string dirVersion)
+        {
+            string pathVersion = Path.Combine(
+                dirVersion,
+                $"version_{DateTime.Now.ToString("yyyyMMddHHmmss")}");
+
+            pathVersion = $"{pathVersion}{file.Extension}";
+
+            FileInfo fileInfoVersion = new FileInfo(pathVersion);
+            if (!fileInfoVersion.Exists)
+            {
+                bool copied = false;
+                for (int i = 0; i < 3; i++)
+                {
+                    try
+                    {
+                        if (CheckHash)
+                            if (FileExistsByHash(dirVersion, file.FullName))
+                                break;
+
+                        file.CopyTo(pathVersion);
+                        copied = true;
+                    }
+                    catch (Exception)
+                    {
+                        Thread.Sleep(1000);
+                    }
+
+                    if (copied)
+                        break;
+                }
+            }
+        }
+
+        private bool FileExistsByHash(string dirVersion, string path)
+        {
+            if (!_controlHash.ContainsKey(dirVersion))
+                _controlHash.Add(dirVersion, new Dictionary<string, string>());
+
+            string currentHash = GetMD5(path);
+
+            if (_controlHash[dirVersion].ContainsKey(currentHash))
+                return true;
+            else
+            {
+                _controlHash[dirVersion].Add(
+                    currentHash,
+                    path);
+                return false;
+            }
+        }
+
+        private string GetMD5(string path)
+        {
+            string hash = string.Empty;
+
+            try
+            {
+                using (MD5 md5 = MD5.Create())
+                {
+                    using (FileStream stream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read))
+                    {
+                        byte[] hashByte = md5.ComputeHash(stream);
+                        hash = BitConverter.ToString(hashByte).Replace("-", "").ToLowerInvariant();
+                        stream.Close();
+                    }
+
+                    md5.Clear();
+                }
+            }
+            catch (FileNotFoundException)
+            {
+            }
+            catch (IOException)
+            {
+            }
+
+            return hash;
+        }
+
+    }
+}
