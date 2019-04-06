f037aa94b32c13222deb9fa515e20a39b2cfb025
djserega <galyuk.sergey@gmail.com>
2019-04-06 11:29:37 +0300

    cr

--- src/DescriptionsTheMethods/Main.cs
+++ src/DescriptionsTheMethods/Main.cs
@@ -8,15 +8,12 @@ using System.Text;
 
 namespace _1CProgrammerAssistant.DescriptionsTheMethods
 {
-    public class Main : INotifyPropertyChanged
+    public class Main
     {
 
         #region Private fields
 
-        private readonly string _prefixString = "\t\t\t";
         private string _stringMethodWithoutDirectiveCompilation;
-
-        private readonly List<ObjectParameter> _parametersMethod = new List<ObjectParameter>();
 
         #endregion
 
@@ -34,17 +31,6 @@ namespace _1CProgrammerAssistant.DescriptionsTheMethods
 
         private bool StringIsFunction { get => _stringMethodWithoutDirectiveCompilation.TrimStart().StartsWith("функция", true, null); }
         private bool StringIsProcedure { get => _stringMethodWithoutDirectiveCompilation.TrimStart().StartsWith("процедура", true, null); }
-        private bool ExportMethod { get => StringMethod.TrimEnd().EndsWith("экспорт", true, null); }
-        private string WrapLineParameters { get; set; }
-        private bool ManyParameters { get => _parametersMethod.Count() > 1; }
-
-        #endregion
-
-        #region Notify property changed
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        public virtual void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
-            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 
         #endregion
 
@@ -76,102 +62,11 @@ namespace _1CProgrammerAssistant.DescriptionsTheMethods
 
         private void CompileDescription()
         {
-            SetParametersMethod();
+            MakingDescription makingDescription = new MakingDescription(StringMethod);
+            makingDescription.Making(StringIsFunction, IncludeStringMethod);
 
-            StringBuilder builderDescription = new StringBuilder();
-
-            builderDescription.AppendLine("// ");
-            builderDescription.AppendLine("//");
-            builderDescription.AppendLine("// Параметры:");
-
-            if (_parametersMethod.Count == 0)
-                builderDescription.AppendLine("//\t\t-  - ");
-            else
-                foreach (ObjectParameter parameter in _parametersMethod)
-                    builderDescription.AppendLine($"//\t\t{parameter.Name} - {parameter.Type} - {parameter.Description}");
-
-            if (StringIsFunction)
-            {
-                builderDescription.AppendLine("//");
-                builderDescription.AppendLine("// Возвращаемое значение:");
-                builderDescription.AppendLine("//\t\t - ");
-            }
-
-            builderDescription.AppendLine("//");
-
-            Description = builderDescription.ToString();
-
-            if (IncludeStringMethod)
-            {
-                if (ManyParameters)
-                {
-                    int positionOpeningBracket = StringMethod.IndexOf("(");
-                    string methodNameBeforeBracket = StringMethod.Substring(0, positionOpeningBracket);
-
-                    Description += $"{methodNameBeforeBracket}({WrapLineParameters})";
-                    Description += ExportMethod ? " Экспорт " : "  ";
-                }
-                else
-                {
-                    Description += StringMethod;
-                }
-            }
-        }
-
-        private void SetParametersMethod()
-        {
-            _parametersMethod.Clear();
-
-            string parser = StringMethod;
-
-            parser = parser.NonUsedStart();
-
-            int countOpeningBracket = parser.Count(f => f == '(');
-            if (countOpeningBracket == 1)
-            {
-                int positionOpeningBracket = parser.IndexOf("(");
-
-                MethodName = parser.Substring(0, positionOpeningBracket);
-
-                parser = parser.Substring(positionOpeningBracket + 1);
-
-                int countClosingBracket = parser.Count(f => f == ')');
-
-                if (countClosingBracket == 1)
-                {
-                    parser = parser.NonUsedEnd();
-
-                    StringBuilder builderWrapParameters = new StringBuilder();
-                    builderWrapParameters.AppendLine();
-
-                    string[] parserParameters = parser.Split(',');
-                    int parametersLeft = parserParameters.Count();
-                    foreach (string itemParameter in parserParameters)
-                    {
-                        if (!string.IsNullOrWhiteSpace(itemParameter))
-                        {
-                            parametersLeft--;
-
-                            builderWrapParameters.Append(_prefixString);
-                            builderWrapParameters.Append(itemParameter.Trim());
-                            builderWrapParameters.AppendLine(parametersLeft > 0 ? "," : "");
-
-                            string paramenterName = itemParameter.TrimStart().RemoveStartText("знач").Trim();
-
-                            int positionEqual = paramenterName.IndexOf('=');
-                            if (positionEqual > 0)
-                                paramenterName = paramenterName.Left(positionEqual).TrimEnd();
-
-                            ObjectParameter objectParameter = new ObjectParameter(paramenterName);
-                            objectParameter.SetTypeByName();
-
-                            _parametersMethod.Add(objectParameter);
-                        }
-                    }
-
-                    WrapLineParameters = builderWrapParameters.ToString().TrimEnd();
-                }
-            }
+            MethodName = makingDescription.MethodName;
+            Description = makingDescription.Description;
         }
 
         #endregion
---
+++ src/DescriptionsTheMethods/MakingDescription.cs
@@ -0,0 +1,136 @@
+﻿using _1CProgrammerAssistant.Additions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1CProgrammerAssistant.DescriptionsTheMethods
+{
+    internal class MakingDescription
+    {
+        private readonly string _prefixString = "\t\t\t";
+        private readonly List<Models.ObjectParameter> _parametersMethod = new List<Models.ObjectParameter>();
+
+        public MakingDescription(string source)
+        {
+            Source = source;
+        }
+
+        internal string Source { get; }
+        internal string Description { get; private set; }
+        internal string MethodName { get; private set; }
+
+        private bool ExportMethod { get => Source.TrimEnd().EndsWith("экспорт", true, null); }
+        private string WrapLineParameters { get; set; }
+
+        private bool ManyParameters { get => _parametersMethod.Count() > 1; }
+
+
+        internal void Making(bool appendReturnValue, bool includeStringMethod)
+        {
+            Description = string.Empty;
+            MethodName = string.Empty;
+
+            CreateParameters();
+            CreateDescription(appendReturnValue, includeStringMethod);
+
+            if (includeStringMethod)
+            {
+                if (ManyParameters)
+                {
+                    int positionOpeningBracket = Source.IndexOf("(");
+                    string methodNameBeforeBracket = Source.Substring(0, positionOpeningBracket);
+
+                    Description += $"{methodNameBeforeBracket}({WrapLineParameters})";
+                    Description += ExportMethod ? " Экспорт " : "  ";
+                }
+                else
+                {
+                    Description += Source;
+                }
+            }
+        }
+
+        private void CreateParameters()
+        {
+            _parametersMethod.Clear();
+
+            string parser = Source;
+
+            parser = parser.NonUsedStart();
+
+            int countOpeningBracket = parser.Count(f => f == '(');
+            if (countOpeningBracket == 1)
+            {
+                int positionOpeningBracket = parser.IndexOf("(");
+
+                MethodName = parser.Substring(0, positionOpeningBracket);
+
+                parser = parser.Substring(positionOpeningBracket + 1);
+
+                int countClosingBracket = parser.Count(f => f == ')');
+
+                if (countClosingBracket == 1)
+                {
+                    parser = parser.NonUsedEnd();
+
+                    StringBuilder builderWrapParameters = new StringBuilder();
+                    builderWrapParameters.AppendLine();
+
+                    string[] parserParameters = parser.Split(',');
+                    int parametersLeft = parserParameters.Count();
+                    foreach (string itemParameter in parserParameters)
+                    {
+                        if (!string.IsNullOrWhiteSpace(itemParameter))
+                        {
+                            parametersLeft--;
+
+                            builderWrapParameters.Append(_prefixString);
+                            builderWrapParameters.Append(itemParameter.Trim());
+                            builderWrapParameters.AppendLine(parametersLeft > 0 ? "," : "");
+
+                            string paramenterName = itemParameter.TrimStart().RemoveStartText("знач").Trim();
+
+                            int positionEqual = paramenterName.IndexOf('=');
+                            if (positionEqual > 0)
+                                paramenterName = paramenterName.Left(positionEqual).TrimEnd();
+
+                            Models.ObjectParameter objectParameter = new Models.ObjectParameter(paramenterName);
+                            objectParameter.SetTypeByName();
+
+                            _parametersMethod.Add(objectParameter);
+                        }
+                    }
+
+                    WrapLineParameters = builderWrapParameters.ToString().TrimEnd();
+                }
+            }
+        }
+
+        private void CreateDescription(bool appendReturnValue, bool includeStringMethod)
+        {
+            StringBuilder builderDescription = new StringBuilder();
+
+            builderDescription.AppendLine("// ");
+            builderDescription.AppendLine("//");
+            builderDescription.AppendLine("// Параметры:");
+
+            if (_parametersMethod.Count == 0)
+                builderDescription.AppendLine("//\t\t-  - ");
+            else
+                foreach (Models.ObjectParameter parameter in _parametersMethod)
+                    builderDescription.AppendLine($"//\t\t{parameter.Name} - {parameter.Type} - {parameter.Description}");
+
+            if (appendReturnValue)
+            {
+                builderDescription.AppendLine("//");
+                builderDescription.AppendLine("// Возвращаемое значение:");
+                builderDescription.AppendLine("//\t\t - ");
+            }
+
+            builderDescription.AppendLine("//");
+
+            Description = builderDescription.ToString();
+        }
+    }
+}
