11a1c833857cf0ca90087f3d638506fcb5b4a329
djserega <galyuk.sergey@gmail.com>
2019-04-13 22:22:27 +0300

    using custom parameter prefixes

--- src/DescriptionsTheMethods/Models/TypeObjectParameter.cs
+++ src/DescriptionsTheMethods/Models/TypeObjectParameter.cs
@@ -1,47 +1,67 @@
-﻿namespace _1CProgrammerAssistant.DescriptionsTheMethods.Models
+﻿using _1CProgrammerAssistant.Additions;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace _1CProgrammerAssistant.DescriptionsTheMethods.Models
 {
     internal class TypeObjectParameter
     {
+        private static Dictionary<string, List<string>> _usersTypeParameters = new Dictionary<string, List<string>>();
+
+        internal TypeObjectParameter()
+        {
+            InitializeUsersTypeParameters();
+        }
+
         internal virtual string Name { get; set; }
         internal virtual string Type { get; set; }
 
         #region Type object
 
-        internal bool IsArray { get => NameStartsWith("мс"); }
-        internal bool IsMap { get => NameStartsWith("со"); }
-        internal bool IsStructure { get => NameStartsWith("ст"); }
-        internal bool IsValueList { get => NameStartsWith("сз"); }
-        internal bool IsValueTable { get => NameStartsWith("тз"); }
-        internal bool IsValueTree { get => NameStartsWith("дз"); }
+        internal bool IsArray { get => CheckStartsWith("мс", "Массив"); }
+        internal bool IsMap { get => CheckStartsWith("со", "Соответствие"); }
+        internal bool IsStructure { get => CheckStartsWith("ст", "Структура"); }
+        internal bool IsValueList { get => CheckStartsWith("сз", "СписокЗначений"); }
+        internal bool IsValueTable { get => CheckStartsWith("тз", "ТаблицаЗначений"); }
+        internal bool IsValueTree { get => CheckStartsWith("дз", "ДеревоЗначений"); }
 
-        internal bool IsFixedArray { get => NameStartsWith("фмс"); }
-        internal bool IsFixedMap { get => NameStartsWith("фсо"); }
-        internal bool IsFixedStructure { get => NameStartsWith("фст"); }
+        internal bool IsFixedArray { get => CheckStartsWith("фмс", "ФиксированныйМассив"); }
+        internal bool IsFixedMap { get => CheckStartsWith("фсо", "ФиксированноеСоответствие"); }
+        internal bool IsFixedStructure { get => CheckStartsWith("фст", "ФиксированнаяСтруктура"); }
 
-        internal bool IsReference { get => NameStartsWith("спр"); }
-        internal bool IsDocument { get => NameStartsWith("док"); }
+        internal bool IsReference { get => CheckStartsWith("спр", "СправочникСсылка"); }
+        internal bool IsDocument { get => CheckStartsWith("док", "ДокументСсылка"); }
 
-        internal bool IsDynamicList { get => NameStartsWith("дс"); }
+        internal bool IsDynamicList { get => CheckStartsWith("дс", "ДинамическийСписок"); }
 
         internal bool IsString
         {
-            get
-            {
-                switch (Name)
-                {
-                    case "ИмяПараметра":
-                    case "Штрихкод":
-                        return true;
-                    default:
-                        return false;
-                }
-            }
+            get => CheckStartsWith("ИмяПараметра", "Строка")
+                || CheckStartsWith("Штрихкод", "Строка");
         }
 
         #endregion
 
-        private bool NameStartsWith(string text) 
-            => Name.StartsWith(text, true, null);
+        private bool CheckStartsWith(string text, string typeName)
+        {
+            bool startWith = Name.StartsWith(text, true, null);
+
+            if (!startWith)
+            {
+                if (_usersTypeParameters.ContainsKey(typeName.ToLower()))
+                {
+                    foreach (string prefixText in _usersTypeParameters[typeName.ToLower()])
+                    {
+                        startWith = Name.StartsWith(prefixText, true, null);
+                        if (startWith)
+                            break;
+                    }
+                }
+            }
+
+            return startWith;
+        }
 
         internal void SetTypeByName()
         {
@@ -74,6 +94,76 @@
 
             else if (IsString)
                 Type = "Строка";
+        }
+
+        private void InitializeUsersTypeParameters()
+        {
+            if (_usersTypeParameters.Count > 0)
+                return;
+
+            var createrType = new ReaderFileTypeParameters("UsersTypeParameters.cfg");
+
+            string textUsersFile = string.Empty;
+            if (createrType.Exist())
+                textUsersFile = createrType.Read();
+            else
+            {
+                createrType.Create(GetDefaultTextUsersTypeParameters());
+                textUsersFile = createrType.Read();
+            }
+
+            ReadTextUserFile(textUsersFile);
+        }
+
+        private string GetDefaultTextUsersTypeParameters()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.AppendLine("### Имя типа :  Префикс параметра");
+            stringBuilder.AppendLine();
+            stringBuilder.AppendLine("Массив : мс;");
+            stringBuilder.AppendLine("Соответствие : со;");
+            stringBuilder.AppendLine("Структура : ст;");
+            stringBuilder.AppendLine("СписокЗначений : сз;");
+            stringBuilder.AppendLine("ТаблицаЗначений : тз;");
+            stringBuilder.AppendLine("ДеревоЗначений : дз;");
+            stringBuilder.AppendLine("ФиксированныйМассив : фмс;");
+            stringBuilder.AppendLine("ФиксированноеСоответствие : фсо;");
+            stringBuilder.AppendLine("ФиксированнаяСтруктура : фст;");
+            stringBuilder.AppendLine("СправочникСсылка : спр;");
+            stringBuilder.AppendLine("ДокументСсылка : док");
+            stringBuilder.AppendLine("ДинамическийСписок : дс");
+            stringBuilder.AppendLine("Строка: ИмяПараметра; Штрихкод;");
+
+            return stringBuilder.ToString();
+        }
+
+        private void ReadTextUserFile(string text)
+        {
+            string[] rows = text.Replace("\r", "").Split('\n');
+            foreach (string typeObject in rows)
+            {
+                if (!typeObject.LeftEquals("###"))
+                {
+                    string[] partTypeObject = typeObject.Split(':');
+
+                    if (partTypeObject.Length == 2)
+                    {
+                        string keyObject = partTypeObject[0];
+
+                        List<string> listPrefix = new List<string>();
+                        string[] valuesObject = partTypeObject[1].Split(';');
+                        foreach (string value in valuesObject)
+                        {
+                            if (!string.IsNullOrWhiteSpace(value))
+                                listPrefix.Add(value.Trim());
+                        }
+
+                        _usersTypeParameters.Add(keyObject.Trim().ToLower(), listPrefix);
+                    }
+                }
+            }
+
         }
 
     }
---
+++ src/DescriptionsTheMethods/ReaderFileTypeParameters.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace _1CProgrammerAssistant.DescriptionsTheMethods
+{
+    internal class ReaderFileTypeParameters
+    {
+        internal ReaderFileTypeParameters(string path)
+        {
+            Path = path;
+        }
+
+        internal string Path { get; }
+
+        internal bool Exist()
+        {
+            return new FileInfo(Path).Exists;
+        }
+
+        internal void Create(string text)
+        {
+            FileInfo fileInfo = new FileInfo(Path);
+            using (StreamWriter streamWriter = fileInfo.CreateText())
+            {
+                streamWriter.Write(text);
+                streamWriter.Close();
+            }
+        }
+
+        internal string Read()
+        {
+            string textInFile = string.Empty;
+
+            using (StreamReader reader = new StreamReader(Path))
+            {
+                textInFile = reader.ReadToEnd();
+                reader.Close();
+            }
+
+            return textInFile;
+        }
+    }
+}
