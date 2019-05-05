0e43fb61e861574db247e5861900f55122f6b6d2
djserega <galyuk.sergey@gmail.com>
2019-05-05 17:43:22 +0300

    added event remove

---
+++ src/MethodStore/Events/RemoveElementStoreEvent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1CProgrammerAssistant.MethodStore.Events
+{
+    public delegate void RemoveElementStore(int id);
+
+    public class RemoveElementStoreEvent : EventArgs
+    {
+        public static event RemoveElementStore RemoveElementStoreEvents;
+        public static void Remove(int id) => RemoveElementStoreEvents?.Invoke(id);
+    }
+}
