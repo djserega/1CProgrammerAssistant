6c957560d1a1f4d8b33233df9e3353da3d3b916d
djserega <galyuk.sergey@gmail.com>
2019-05-05 17:29:00 +0300

    added method Fill in element store

--- src/MethodStore/Models/ElementStore.cs
+++ src/MethodStore/Models/ElementStore.cs
@@ -13,6 +13,24 @@ namespace _1CProgrammerAssistant.MethodStore.Models
         public string Method { get; set; } = string.Empty;
         public string Comment { get; set; } = string.Empty;
 
+        public void Fill(ElementStore elementStore)
+        {
+            if (!Group.Equals(elementStore.Group))
+                Group = elementStore.Group;
+
+            if (!Type.Equals(elementStore.Type))
+                Type = elementStore.Type;
+
+            if (!Module.Equals(elementStore.Module))
+                Module = elementStore.Module;
+
+            if (!Method.Equals(elementStore.Method))
+                Method = elementStore.Method;
+
+            if (!Comment.Equals(elementStore.Comment))
+                Comment = elementStore.Comment;
+        }
+
         public void Save()
         {
             Events.UpdateElementStoreEvent.Update(this);
