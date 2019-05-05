e823e0bae65360c6fe9a969359a50012e627af76
djserega <galyuk.sergey@gmail.com>
2019-05-05 17:29:50 +0300

    loading element store by id

---
+++ src/MethodStore/Events/LoadElementStoreEvent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1CProgrammerAssistant.MethodStore.Events
+{
+    public delegate Models.ElementStore LoadElementStore(int id);
+
+    public class LoadElementStoreEvent : EventArgs
+    {
+        public static event LoadElementStore LoadElementStoreEvents;
+
+        public static Models.ElementStore Load(int id)
+        {
+            return LoadElementStoreEvents?.Invoke(id);
+        }
+    }
+}
