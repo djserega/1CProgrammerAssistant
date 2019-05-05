53dc03860afbc97caff4527a9aecf7f062038146
djserega <galyuk.sergey@gmail.com>
2019-05-05 16:11:30 +0300

    added events work with methodstore.db

---
+++ src/MethodStore/Events/LoadElementsStoreEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1CProgrammerAssistant.MethodStore.Events
+{
+    public delegate List<Models.ElementStore> LoadElementsStore();
+    public class LoadElementsStoreEvent : EventArgs
+    {
+        public static event LoadElementsStore LoadElementsStoreEvents;
+        internal static List<Models.ElementStore> Load()
+        {
+            return LoadElementsStoreEvents?.Invoke();
+        }
+    }
+}
---
+++ src/MethodStore/Events/UpdateElementStoreEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1CProgrammerAssistant.MethodStore.Events
+{
+    public delegate void UpdateElementStore(Models.ElementStore elementStore);
+
+    public class UpdateElementStoreEvent : EventArgs
+    {
+        public static event UpdateElementStore UpdateElementStoreEvents;
+        internal static void Update(Models.ElementStore elementStore) => UpdateElementStoreEvents?.Invoke(elementStore);
+    }
+}
